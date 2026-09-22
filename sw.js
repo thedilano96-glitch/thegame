b5f6525d745557a7ad6d7cb2560f8754f0951d00
thedilano96-glitch <thedilano96@gmail.com>
2026-09-22 18:00:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,7 @@
-const CACHE_NAME = 'thegame-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './thegame.js',
-  './manifest.json'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
 });
